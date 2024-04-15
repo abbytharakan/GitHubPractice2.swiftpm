444383fc6abfd268250e4791699ac1e3f5ef9f86
abbytharakan <atharakan5365@stu.d214.org>
2024-04-15 14:19:28 -0500

    Added NavigationLink/View for NounView

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationView {
+            NavigationLink ("Click"){
+                NounView()
+            }
+        }
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
