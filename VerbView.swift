66006edf7cb093c56c4307816af7775681161180
abbytharakan <atharakan5365@stu.d214.org>
2024-04-15 14:25:38 -0500

    navigation link for adjview

---
+++ VerbView.swift
@@ -0,0 +1,18 @@
+//
+//  VerbView.swift
+//  GitHubPractice2
+//
+//  Created by Abby Tharakan on 4/15/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct VerbView: View {
+    var body: some View {
+        NavigationLink("Go To The AdjectiveView"){
+            AdjectiveView()
+        }
+    }
+}
