9ecf40cd9142e4f82cc0a5f0e5b4742b39a2d33d
Adam <ajakubiec7173@stu.d214.org>
2024-04-15 14:19:12 -0500

    Added View

---
+++ NounView.swift
@@ -0,0 +1,15 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Adam Jakubiec on 4/15/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct NounView: View {
+    var body: some View {
+        Text("Noun View")
+    }
+}
