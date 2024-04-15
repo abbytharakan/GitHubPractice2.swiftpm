688626c6bb9d608d7a4146ed1c05e56c75a6d094
abbytharakan <atharakan5365@stu.d214.org>
2024-04-15 14:40:37 -0500

    SummaryView Texts

---
+++ SummaryView.swift
@@ -0,0 +1,16 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Abby Tharakan on 4/15/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text("The playful dog went to the park.")
+    }
+}
